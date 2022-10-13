92290ae3e7cd254244780dbe2201aa8378a5f60f
angel <angel@Gawra>
2022-10-13 18:53:32 +0200

    Ejercicio entrega terminado

--- EjerciciosProcesosJava/src/ejercicio1/Division.java
+++ EjerciciosProcesosJava/src/ejercicio1/Division.java
@@ -10,7 +10,7 @@ import java.io.*;
  * @author alu
  *
  */
-public class LectorTeclado {
+public class Division {
 
 	/**
 	 * @param args
@@ -20,23 +20,25 @@ public class LectorTeclado {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String entrada = "";
 		try {
-			System.out.println("Primer Número:");
+			System.out.println("Primer Numero:");
 			entrada = br.readLine();
 			int num1 = Integer.parseInt(entrada);
 			
-			System.out.println("Segundo Número:");
+			System.out.println("Segundo Numero:");
 			entrada = br.readLine();
 			int num2 = Integer.parseInt(entrada);
-			int division = 0;
+			double division = 0;
 			try {
-				division = num1/num2;
+				division = (double)num1/num2;
+				System.out.println("La division de " + num1 + "/" + num2 +" es: " + division);
 			}catch(ArithmeticException AE) {
 				AE.printStackTrace();
 			}
-			System.out.println("La division de " + num1 + "/" + num2 +" es: " + division);
 			br.close();
-		} catch (IOException ioe) {
+		}catch (IOException ioe) {
 			ioe.printStackTrace();
+		}catch (NumberFormatException nfe) {
+			nfe.printStackTrace();
 		}
 		
 
